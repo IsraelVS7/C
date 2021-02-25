430f421ab4b95e1cd1a54e1181fd79a273cf56ea
IsraelVS7 <78622650+IsraelVS7@users.noreply.github.com>
2021-02-25 10:47:22 -0600

    Add files via upload

---
+++ HOMEWORK/HOMEWORK_1.c
@@ -0,0 +1,55 @@
+// i dont work 
+
+
+#include <stdio.h>
+#include <string.h>
+
+#define LEN 10000
+#define COMP1 1
+#define COMP2 0
+
+
+
+int main() {
+int LONG;
+int I, X;
+  
+char IN[LEN];
+char OUT[LEN];
+char A;
+      
+ printf(" TEXT: ");
+ gets(IN);        
+ LONG  = strlen(IN);
+
+  for (I = 0; I<LONG ; ++I) {
+    
+        if (IN[I] == '<')
+        {  
+        X = COMP1;
+        }
+        if (IN[I] == '>')
+        {
+        X = COMP2;
+        }
+        if (X == COMP2)
+        {
+        OUT[I] = IN[I]; 
+        }
+    
+        if (OUT[I] == '>') 
+        {
+        OUT[I] = ' ';
+        }
+        
+        
+        
+  while ((A = getchar));
+	    {
+		if (A == ' ')
+		{
+			printf("_");
+		} 
+	    }
+ }
+}
